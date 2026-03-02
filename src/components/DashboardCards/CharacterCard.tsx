41dba93fa58cd216b70cd84d4e3fd02227f8ac88
czifumasa <lukasz.lenart912@gmail.com>
2026-03-02 23:57:49 +0100

    Combine Character and League cards into single one

--- src/components/DashboardCards/CharacterCard.tsx
+++ src/components/DashboardCards/CharacterCard.tsx
@@ -3,19 +3,34 @@ import { JSX } from 'react';
 import './DashboardCards.css';
 
 interface CharacterCardProps {
+	leagueName: string;
+	leagueDetail: string;
 	characterName: string;
 	characterDetail: string;
-	onConfigure: () => void;
+	onSync: () => void;
 }
 
-export function CharacterCard({ characterName, characterDetail, onConfigure }: CharacterCardProps): JSX.Element {
+export function CharacterCard({
+	leagueName,
+	leagueDetail,
+	characterName,
+	characterDetail,
+	onSync,
+}: CharacterCardProps): JSX.Element {
 	return (
-		<div className="dashboardCard">
-			<span className="dashboardCardLabel">Character</span>
-			<span className="dashboardCardValue">{characterName}</span>
-			<span className="dashboardCardDetail">{characterDetail}</span>
-			<button type="button" className="dashboardCardConfigureButton" onClick={onConfigure}>
-				Configure
+		<div className="dashboardCard characterCard">
+			<div className="characterCardSection">
+				<span className="dashboardCardLabel">League</span>
+				<span className="dashboardCardValue">{leagueName}</span>
+				<span className="dashboardCardDetail">{leagueDetail}</span>
+			</div>
+			<div className="characterCardSection">
+				<span className="dashboardCardLabel">Character</span>
+				<span className="dashboardCardValue">{characterName}</span>
+				<span className="dashboardCardDetail">{characterDetail}</span>
+			</div>
+			<button type="button" className="characterCardSyncButton" onClick={onSync}>
+				Sync
 			</button>
 		</div>
 	);
